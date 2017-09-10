ade0e8a724a2bfcae31ab09a26403eb1bfc4c6ed
zicla <ziclax@126.com>
2017-09-10 21:20:30 +0800

    Finish OO1.

--- 000to100/001_two_sum.go
+++ 000to100/001_two_sum.go
@@ -6,12 +6,24 @@ func twoSum(nums []int, target int) []int {
 
 	N := len(nums)
 
-	for start := 0; start <= N-2; start++ {
+	dict := make(map[int]int)
+	for i := 0; i < N; i++ {
 
-		for end := start + 1; end <= N-1; end++ {
+		//解决两个相同数字组成的。
+		old, ok := dict[nums[i]]
+		if ok && target%2 == 0 && target/2 == nums[i] {
+			return []int{old, i}
+		}
 
-			if nums[start]+nums[end] == target {
-				return []int{start, end}
+		dict[nums[i]] = i
+	}
+
+	for start := 0; start < N; start++ {
+		positive := target - nums[start]
+		if positive != nums[start] {
+			i, ok := dict[positive]
+			if ok {
+				return []int{start, i}
 			}
 		}
 	}
@@ -22,7 +34,7 @@ func main() {
 
 	nums := []int{2, 7, 11, 15}
 
-	target := 18
+	target := 22
 
 	fmt.Printf("%v", twoSum(nums, target))
 }
