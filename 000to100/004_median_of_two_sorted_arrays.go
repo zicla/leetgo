b4e5d4ed6e3fac1e089cfb53954d94038dabd99e
zicla <lish516@126.com>
2017-10-12 11:27:22 +0800

    Finish the problem 004.

--- 000to100/004_median_of_two_sorted_arrays.go
+++ 000to100/004_median_of_two_sorted_arrays.go
@@ -7,79 +7,57 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	m := len(nums1)
 	n := len(nums2)
 
-	//odd
-	p1 := 0
-	p2 := 0
-
-	count := 1
-
-	if (m+n)%2 == 1 {
-		target := (m + n + 1) / 2
-		for true {
-
-			var current int;
-			//nums1 end.
-			if p1 >= m {
-				current = nums2[p2]
-				p2++
-			} else if p2 >= n {
-				current = nums1[p1]
-				p1++
-			} else {
-				if nums1[p1] > nums2[p2] {
-					current = nums2[p2]
-					p2++
-				} else {
-					current = nums1[p1]
-					p1++
-				}
-			}
-
-			if count == target {
-				return float64(current)
-			}
-
-			count++
-		}
-
+	if (m+n)%2 == 0 {
+		v1 := findKth(nums1, 0, m, nums2, 0, n, (m+n)/2)
+		v2 := findKth(nums1, 0, m, nums2, 0, n, (m+n)/2+1)
+		return float64(v1+v2) / 2.0
 	} else {
-		target := (m + n) / 2
-		var seed1 int
-		var seed2 int
-		//even
-		for true {
-
-			var current int;
-			//nums1 end.
-			if p1 >= m {
-				current = nums2[p2]
-				p2++
-			} else if p2 >= n {
-				current = nums1[p1]
-				p1++
-			} else {
-				if nums1[p1] > nums2[p2] {
-					current = nums2[p2]
-					p2++
-				} else {
-					current = nums1[p1]
-					p1++
-				}
-			}
-
-			if count == target {
-				seed1 = current
-			} else if count == target+1 {
-				seed2 = current
-				break
-			}
-
-			count++
-		}
-		return (float64(seed1) + float64(seed2)) / 2
+		return float64(findKth(nums1, 0, m, nums2, 0, n, (m+n)/2+1))
 	}
 
-	return 0;
+}
+
+func min(a int, b int) int {
+
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+
+}
+
+//m is the length of array1.
+func findKth(nums1 []int, startIndex1 int, m int, nums2 []int, startIndex2 int, n int, k int) int {
+
+	//assume m < n.
+	if m > n {
+		return findKth(nums2, startIndex2, n, nums1, startIndex1, m, k)
+	}
+
+	if m == 0 {
+		return nums2[startIndex2+k-1]
+	}
+
+	if k == 1 {
+		return min(nums1[startIndex1], nums2[startIndex2])
+	}
+
+	pa := min(k/2, m)
+	pb := k - pa
+
+	pivot1 := nums1[startIndex1+pa-1]
+	pivot2 := nums2[startIndex2+pb-1]
+	if pivot1 < pivot2 {
+		return findKth(nums1, startIndex1+pa, m-pa, nums2, startIndex2, n, k-pa)
+	} else if pivot1 > pivot2 {
+		return findKth(nums1, startIndex1, m, nums2, startIndex2+pb, n-pb, k-pb)
+	} else {
+		return pivot1
+	}
+
+	return 0
+
 }
 func main() {
 
@@ -88,7 +66,7 @@ func main() {
 
 	fmt.Println(findMedianSortedArrays(num1, num2))
 
-	num3 := []int{1, 3}
+	num3 := []int{1, 3, 8}
 	num4 := []int{8}
 
 	fmt.Println(findMedianSortedArrays(num3, num4))
