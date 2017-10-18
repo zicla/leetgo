bfb0e305e00c6b875a424de0789b38f261020a4c
zicla <lish516@126.com>
2017-10-18 17:26:57 +0800

    Finish the 084 question.

--- 001to100/084_largest_rectangle_in_histogram.go
+++ 001to100/084_largest_rectangle_in_histogram.go
@@ -2,11 +2,43 @@ package main
 
 import "fmt"
 
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
 func largestRectangleArea(heights []int) int {
 
-	N := len(heights)
+	arr := append(heights, 0)
+	N := len(arr)
+	stack := make([]int, N)
+	topIndex := -1
+	area := 0
+	for i := 0; i < N; i++ {
+		item := arr[i]
+		//入栈
+		if topIndex == -1 || arr[stack[topIndex]] <= item {
+			topIndex++
+			stack[topIndex] = i
+		} else {
 
-	return N
+			//出栈。
+			top := stack[topIndex]
+			topIndex--
+
+			//最后一个元素出栈时，宽度为全部。
+			if topIndex == -1 {
+				area = max(area, arr[top]*i)
+			} else {
+				area = max(area, arr[top]*(i-stack[topIndex]-1))
+			}
+			i--
+		}
+	}
+	return area
 }
 
 func main() {
@@ -24,6 +56,18 @@ func main() {
 	fmt.Println(largestRectangleArea(matrix))
 
 	matrix = []int{2, 1, 2}
+
+	fmt.Println(largestRectangleArea(matrix))
+
+	matrix = []int{5, 4, 1, 2}
+
+	fmt.Println(largestRectangleArea(matrix))
+
+	matrix = []int{4, 2, 0, 3, 2, 5}
+
+	fmt.Println(largestRectangleArea(matrix))
+
+	matrix = []int{3,6,5,7,4,8,1,0}
 
 	fmt.Println(largestRectangleArea(matrix))
 
