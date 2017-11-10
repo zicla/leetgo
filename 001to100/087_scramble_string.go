fb36ec6d1bd76337d77ff83ab1b7d765f24e90fc
zicla <lish516@126.com>
2017-11-10 11:10:01 +0800

    Fix the null bug of gorm.

--- 001to100/087_scramble_string.go
+++ 001to100/087_scramble_string.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func isScramble(s1 string, s2 string) bool {
 
 	return true
@@ -7,6 +9,14 @@ func isScramble(s1 string, s2 string) bool {
 
 func main() {
 
+	var arr [][]int
+	for i := 0; i < 10; i++ {
+		subArr1 := make([]int, 0, 10)
+		for j := 0; j < 5; j++ {
+			subArr1 = append(subArr1, j)
+		}
+		arr = append(arr, subArr1)
+	}
 
-
+	fmt.Printf("%v", arr)
 }
