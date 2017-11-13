ec846b35c73288e368c7e5f9f829d5989d604644
zicla <lish516@126.com>
2017-11-13 14:31:21 +0800

    Finish issue 091.

--- 001to100/091_decode_ways.go
+++ 001to100/091_decode_ways.go
@@ -3,63 +3,64 @@ package main
 import "fmt"
 
 func numDecodings(s string) int {
-	return numDecodingsRecursive(s, 0, len(s)-1)
-}
-
-func numDecodingsRecursive(s string, start int, end int) int {
-	if start > end {
-		return 0
-	}
-	if start == end {
-		if s[start] == '0' {
-			return 0
-		} else {
-			return 1
-		}
-	}
-
-	ch0 := s[start]
-	ch1 := s[end-1]
-	ch2 := s[end]
-
-	if ch0 == '0' {
+	N := len(s)
+	if N == 0 {
 		return 0
 	}
 
-	if ch1 == '0' && ch2 == '0' {
-		return 0
-	}
+	F := make([]int, N, N)
+	for i := 0; i < N; i++ {
 
-	if ch2 == '0' {
-
-		if ch1 == '1' || ch1 == '2' {
-
-			if end-start == 1 {
-				return 1
-			}
-
-			pre := numDecodingsRecursive(s, start, end-2)
-			if pre == 0 {
+		if i == 0 {
+			if s[0] == '0' {
+				F[0] = 0
 				return 0
+			} else {
+				F[0] = 1
+				continue
 			}
-			return pre
+		} else if i == 1 {
+			if s[i] == '0' {
+				if s[i-1] == '1' || s[i-1] == '2' {
+					F[i] = 1
+				} else {
+					return 0
+				}
+			} else {
+				if s[i-1] == '1' || (s[i-1] == '2' && (s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6')) {
+					F[i] = 2
+				} else {
+					F[i] = 1
+				}
+			}
 		} else {
-			return 0
+			if s[i] == '0' {
+				if s[i-1] == '1' || s[i-1] == '2' {
+					//F[i] = F[i-2]*2 + F[i-1] - F[i-2] //因为F[i-2]*2 + F[i-1]正好重复了 F[i-2]
+					F[i] = F[i-2]
+				} else {
+					return 0
+				}
+			} else {
+				if s[i-1] == '0' {
+					F[i] = F[i-1]
+				} else if s[i-1] == '1' {
+					F[i] = F[i-2] + F[i-1]
+				} else if s[i-1] == '2' {
+					if s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6' {
+						F[i] = F[i-2] + F[i-1]
+					} else {
+						F[i] = F[i-1]
+					}
+				} else {
+					F[i] = F[i-1]
+				}
+
+			}
 		}
 
-	} else if ch1 == '1' || (ch1 == '2' && (ch2 == '1' || ch2 == '2' || ch2 == '3' || ch2 == '4' || ch2 == '5' || ch2 == '6')) {
-		pre := numDecodingsRecursive(s, start, end-1)
-		if pre == 0 {
-			return 0
-		}
-		return pre + 1
-	} else {
-		pre := numDecodingsRecursive(s, start, end-1)
-		if pre == 0 {
-			return 0
-		}
-		return pre
 	}
+	return F[N-1]
 
 }
 
@@ -73,7 +74,9 @@ func main() {
 	fmt.Printf("%v\n", numDecodings("01"))
 	fmt.Printf("%v\n", numDecodings("012"))
 	fmt.Printf("%v\n", numDecodings("100"))
+
 	fmt.Printf("%v\n", numDecodings("110"))
+
 	fmt.Printf("%v\n", numDecodings("230"))
 	fmt.Printf("%v\n", numDecodings("12120"))
 	fmt.Printf("%v\n", numDecodings("1212"))
