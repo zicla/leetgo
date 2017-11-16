4c12e2b3919be6c6b820bd3640dd720bd121540a
zicla <lish516@126.com>
2017-11-16 19:03:13 +0800

    Finish the issue of 096.

--- 001to100/096_Unique_Binary_Search_Trees.go
+++ 001to100/096_Unique_Binary_Search_Trees.go
@@ -7,66 +7,21 @@ func numTrees(n int) int {
 	F := make([]int, n+1)
 
 	for i := 1; i <= n; i++ {
-
 		for k := 1; k <= i; k++ {
-
-
-
-		}
-		//if leftTrees == 0 && rightTrees == 0 {
-		//	trees += 1
-		//} else if leftTrees == 0 {
-		//
-		//	trees += rightTrees
-		//
-		//} else if rightTrees == 0 {
-		//	trees += leftTrees
-		//} else {
-		//	trees += leftTrees * rightTrees
-		//}
-
-	}
-
-	return F[n]
-}
-
-func numTreesRecursive(arr []int) int {
-
-	var trees int
-	for _, v := range arr {
-		var newLeftArr []int
-		var newRightArr []int
-		for _, v1 := range arr {
-			if v1 < v {
-				newLeftArr = append(newLeftArr, v1)
-			} else if v1 > v {
-				newRightArr = append(newRightArr, v1)
+			leftNodes := k - 1
+			rightNodes := i - 1 - (k - 1)
+			if leftNodes == 0 && rightNodes == 0 {
+				F[i] += 1
+			} else if leftNodes == 0 {
+				F[i] += F[rightNodes]
+			} else if rightNodes == 0 {
+				F[i] += F[leftNodes]
+			} else {
+				F[i] += F[leftNodes] * F[rightNodes]
 			}
 		}
-
-		var leftTrees int
-		if len(newLeftArr) > 0 {
-			leftTrees = numTreesRecursive(newLeftArr)
-		}
-		var rightTrees int
-		if len(newRightArr) > 0 {
-			rightTrees = numTreesRecursive(newRightArr)
-		}
-
-		if leftTrees == 0 && rightTrees == 0 {
-			trees += 1
-		} else if leftTrees == 0 {
-
-			trees += rightTrees
-
-		} else if rightTrees == 0 {
-			trees += leftTrees
-		} else {
-			trees += leftTrees * rightTrees
-		}
-
 	}
-	return trees
+	return F[n]
 }
 
 func main() {
