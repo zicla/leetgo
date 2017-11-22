149234bde4820ff99acacaf24b534d5e9e42026f
zicla <lish516@126.com>
2017-11-22 15:16:19 +0800

    Ready to solve 108.

---
+++ 101to200/108_Convert_Sorted_Array_to_Binary_Search_Tree.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	. "leetgo/structure/btree"
+)
+
+func sortedArrayToBST(nums []int) *TreeNode {
+
+	return nil
+}
+
+func main() {
+
+
+	PrintLevelBinaryTree(sortedArrayToBST([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))
+}
---
+++ 101to200/108_Convert_Sorted_Array_to_Binary_Search_Tree.md
@@ -0,0 +1,3 @@
+108. Convert Sorted Array to Binary Search Tree
+
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
