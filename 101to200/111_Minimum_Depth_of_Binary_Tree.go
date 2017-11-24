4a9298167da0382d8c3a7fb958b9783691b2851d
zicla <lish516@126.com>
2017-11-24 18:30:30 +0800

    Finish issue 111.

---
+++ 101to200/111_Minimum_Depth_of_Binary_Tree.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	. "leetgo/structure/btree"
+	"fmt"
+	"math"
+)
+
+func minDepth(root *TreeNode) int {
+
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil {
+		return 1 + minDepth(root.Right)
+	}
+	if root.Right == nil {
+		return 1 + minDepth(root.Left)
+	}
+
+	ml := minDepth(root.Left)
+	mr := minDepth(root.Right)
+
+	if ml < mr {
+		return ml + 1
+	} else {
+		return mr + 1
+	}
+
+}
+
+func main() {
+
+	tree := CreateBinaryTree([]int{1, 2, math.MinInt64, 3}, 0)
+	fmt.Println(minDepth(tree))
+}
