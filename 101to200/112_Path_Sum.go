e8f27ff504276dcbd5f41bf9f8a8240a1f1f5201
zicla <lish516@126.com>
2017-11-24 18:51:31 +0800

    Finish issue 112.

---
+++ 101to200/112_Path_Sum.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	. "leetgo/structure/btree"
+	"fmt"
+)
+
+func hasPathSum(root *TreeNode, sum int) bool {
+
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root.Val == sum
+	} else if root.Left == nil {
+		return hasPathSum(root.Right, sum-root.Val)
+	} else if root.Right == nil {
+		return hasPathSum(root.Left, sum-root.Val)
+	} else {
+		return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+	}
+}
+
+func main() {
+
+	tree := CreateBinaryTree([]int{1, 2}, 0)
+	fmt.Println(hasPathSum(tree, 1))
+}
