7f5a48098a2774ef72f72f861f40b2e56379c085
Zic <logzcx@126.com>
2017-11-26 00:03:56 +0800

    Use the triangle as the dp array.

--- 101to200/120_Triangle.go
+++ 101to200/120_Triangle.go
@@ -10,35 +10,29 @@ func minimumTotal(triangle [][]int) int {
 		return 0
 	}
 
-	dp := make([][]int, N)
-	for i := 0; i < N; i++ {
-		dp[i] = make([]int, i+1)
-	}
-
 	for i := 0; i < N; i++ {
 		for j := 0; j <= i; j++ {
 			if i == 0 {
-				dp[0][0] = triangle[0][0]
 			} else {
 				if j == 0 {
-					dp[i][j] = dp[i-1][j] + triangle[i][j]
+					triangle[i][j] = triangle[i-1][j] + triangle[i][j]
 				} else if j == i {
-					dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+					triangle[i][j] = triangle[i-1][j-1] + triangle[i][j]
 				} else {
-					if dp[i-1][j-1] < dp[i-1][j] {
-						dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+					if triangle[i-1][j-1] < triangle[i-1][j] {
+						triangle[i][j] = triangle[i-1][j-1] + triangle[i][j]
 					} else {
-						dp[i][j] = dp[i-1][j] + triangle[i][j]
+						triangle[i][j] = triangle[i-1][j] + triangle[i][j]
 					}
 				}
 			}
 		}
 	}
 
-	min := dp[N-1][0]
+	min := triangle[N-1][0]
 	for i := 1; i < N; i++ {
-		if dp[N-1][i] < min {
-			min = dp[N-1][i]
+		if triangle[N-1][i] < min {
+			min = triangle[N-1][i]
 		}
 	}
 
