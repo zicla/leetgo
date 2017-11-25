48705a380e80b12bfa437ffc15359831a605ed01
Zic <logzcx@126.com>
2017-11-25 22:43:38 +0800

    Finish 118.

---
+++ 101to200/118_Pascal_s_Triangle.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func generate(numRows int) [][]int {
+	var res = make([][]int, numRows)
+	if numRows == 0 {
+		return res
+	}
+
+	res[0] = []int{1}
+	for i := 2; i <= numRows; i++ {
+
+		preRow := res[i-2]
+		row := make([]int, i)
+		row[0] = 1
+		row[i-1] = 1
+		for j := 1; j < i-1; j++ {
+			row[j] = preRow[j-1] + preRow[j]
+		}
+		res[i-1] = row
+	}
+
+	return res
+}
+
+func main() {
+
+	fmt.Printf("%v\n", generate(0))
+	fmt.Printf("%v\n", generate(1))
+	fmt.Printf("%v\n", generate(2))
+	fmt.Printf("%v\n", generate(3))
+	fmt.Printf("%v\n", generate(10))
+}
