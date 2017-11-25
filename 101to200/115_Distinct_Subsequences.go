ed09f567e34509050bd69545de1399d981f00ef5
Zic <logzcx@126.com>
2017-11-25 16:38:22 +0800

    Using dp to solve issue 115.

--- 101to200/115_Distinct_Subsequences.go
+++ 101to200/115_Distinct_Subsequences.go
@@ -6,30 +6,37 @@ import (
 
 func numDistinct(s string, t string) int {
 
-	total := 0
-	numDistinctRecursive(s, len(s), 0, t, len(t), 0, &total)
-	return total
-}
+	//第一个位置表示s的前i个字符，第二个位置表示t的前j个字符。
+	sLen := len(s)
+	tLen := len(t)
 
-func numDistinctRecursive(s string, sLen int, sIndex int, t string, tLen int, tIndex int, total *int) {
-
-	if tIndex == tLen {
-		*total++
-		return
-	}
-	if sIndex == sLen {
-		return
+	var dp [][]int
+	for i := 0; i <= sLen; i++ {
+		row := make([]int, tLen+1)
+		dp = append(dp, row)
 	}
 
-	if s[sIndex] == t[tIndex] {
-		//s继续前进，t按兵不动
-		numDistinctRecursive(s, sLen, sIndex+1, t, tLen, tIndex, total)
-		//s和t同时前进
-		numDistinctRecursive(s, sLen, sIndex+1, t, tLen, tIndex+1, total)
-	} else {
-		//s继续前进，t按兵不动
-		numDistinctRecursive(s, sLen, sIndex+1, t, tLen, tIndex, total)
+	//空串正好是空串的子字符串
+	for i := 0; i < sLen+1; i++ {
+		dp[i][0] = 1
 	}
+	for j := 1; j < tLen+1; j++ {
+		dp[0][j] = 0
+	}
+
+	for i := 1; i < sLen+1; i++ {
+		for j := 1; j < tLen+1; j++ {
+
+			if s[i-1] == t[j-1] {
+				//考虑是否用上母串中的第i个字符。
+				dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[sLen][tLen]
 }
 
 func main() {
