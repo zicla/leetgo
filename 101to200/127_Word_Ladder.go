f3afa4f3fda2141a78ea2a480594d0e009ee1189
zicla <lish516@126.com>
2017-11-27 19:12:19 +0800

    Still not finish 127 yet.

--- 101to200/127_Word_Ladder.go
+++ 101to200/127_Word_Ladder.go
@@ -5,61 +5,6 @@ import (
 	"math"
 )
 
-func Dijkstra(matrix [][]int, vs int) []int {
-	const INF = math.MaxInt32
-
-	vexnum := len(matrix)
-	dist := make([]int, vexnum)
-	// flag[i]=true表示"顶点vs"到"顶点i"的最短路径已成功获取。用此变量可以区分S和U集合。
-	flag := make([]bool, vexnum)
-
-	// 初始化
-	for i := 0; i < vexnum; i++ {
-		// 顶点i的最短路径还没获取到。
-		flag[i] = false
-
-		// 顶点i的最短路径为"顶点vs"到"顶点i"的权。
-		dist[i] = matrix[vs][i];
-	}
-
-	// 对"顶点vs"自身进行初始化
-	flag[vs] = true
-	dist[vs] = 0
-
-	// 遍历G.vexnum-1次；每次找出一个顶点的最短路径。
-	for i := 1; i < vexnum; i++ {
-		// 寻找当前最小的路径；
-		// 即，在未获取最短路径的顶点中，找到离vs最近的顶点 k。
-		min := INF
-		var k int
-		for j := 0; j < vexnum; j++ {
-			if flag[j] == false {
-				if dist[j] < min {
-					min = dist[j]
-					k = j
-				}
-			}
-		}
-		// 标记"顶点k"为已经获取到最短路径
-		flag[k] = true
-		// 修正当前最短路径和前驱顶点
-		// 即，当已经"顶点k的最短路径"之后，更新"未获取最短路径的顶点的最短路径和前驱顶点"
-		for j := 0; j < vexnum; j++ {
-			var tmp = INF
-			if matrix[k][j] != INF {
-				tmp = min + matrix[k][j]
-			}
-			if flag[j] == false {
-				if tmp < dist[j] {
-					dist[j] = tmp
-				}
-			}
-		}
-	}
-
-	return dist
-}
-
 func ladderLength(beginWord string, endWord string, wordList []string) int {
 
 	const INF = math.MaxInt32
@@ -86,7 +31,6 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 	if !endWordAppear {
 		wordList = append(wordList, endWord)
 		endWordIndex = len(wordList) - 1
-
 		return 0
 	}
 
@@ -122,31 +66,34 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 		}
 	}
 
+	//可以看作是走迷宫的题目，使用深度优先搜索可以快速获取最短路径，使用广度优先搜索可以获取具体每一条路的策略。
+	//d表示到每个其他节点的距离。
+	d := make([]int, N)
+	for i := 0; i < N; i++ {
+		d[i] = INF
+	}
+	visited := make([]bool, N)
 
-	//floyed开始你的表演 实时证明floyed会超时。
-	//for k := 0; k < N; k++ {
-	//	for i := 0; i < N; i++ {
-	//		for j := 0; j < N; j++ {
-	//			if matrix[i][j] > matrix[i][k]+matrix[k][j] {
-	//				matrix[i][j] = matrix[i][k] + matrix[k][j];
-	//			}
-	//		}
-	//	}
-	//}
-	//if matrix[beginWordIndex][endWordIndex] == INF {
-	//	return 0
-	//} else {
-	//	return matrix[beginWordIndex][endWordIndex] + 1
-	//}
+	d[beginWordIndex] = 0
+	visited[beginWordIndex] = true
 
-	//Dijkstra开始你的表演
-	list := Dijkstra(matrix, beginWordIndex)
-	if list[endWordIndex] == INF {
-		return 0
-	} else {
-		return list[endWordIndex] + 1
+	curPath := 0
+
+	for i := 0; i <= N; i++ {
+		if d[i] == curPath {
+			i := beginWordIndex
+			for j := 0; j < N; j++ {
+				if !visited[j] {
+					visited[j] = true
+					if matrix[i][j] == 1 {
+						d[j] = curPath + 1
+					}
+				}
+			}
+		}
 	}
 
+	return endWordIndex;
 }
 
 func main() {
