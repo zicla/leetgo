75a0ccd28ba66d1c9b87b6d7ac529b4e636a33cc
zicla <lish516@126.com>
2017-11-27 11:52:16 +0800

    Finish the dijkstra algorithm.

--- structure/graph/dijkstra.go
+++ structure/graph/dijkstra.go
@@ -1,59 +1,73 @@
 package graph
 
+import (
+	"math"
+)
+
+const INF = math.MaxInt32
+
 //dijkstra算法
 //参考文章：http://www.cnblogs.com/skywang12345/p/3711516.html
-func Dijkstra(matrix [][]int, v0 int) []int {
-	//int i,j,k;
-	//int min;
-	//int tmp;
-	//int flag[MAX];      // flag[i]=1表示"顶点vs"到"顶点i"的最短路径已成功获取。
-	//
-	//// 初始化
-	//for (i = 0; i < G.vexnum; i++)
-	//{
-	//flag[i] = 0;              // 顶点i的最短路径还没获取到。
-	//prev[i] = 0;              // 顶点i的前驱顶点为0。
-	//dist[i] = G.matrix[vs][i];// 顶点i的最短路径为"顶点vs"到"顶点i"的权。
-	//}
-	//
-	//// 对"顶点vs"自身进行初始化
-	//flag[vs] = 1;
-	//dist[vs] = 0;
-	//
-	//// 遍历G.vexnum-1次；每次找出一个顶点的最短路径。
-	//for (i = 1; i < G.vexnum; i++)
-	//{
-	//// 寻找当前最小的路径；
-	//// 即，在未获取最短路径的顶点中，找到离vs最近的顶点(k)。
-	//min = INF;
-	//for (j = 0; j < G.vexnum; j++)
-	//{
-	//if (flag[j]==0 && dist[j]<min)
-	//{
-	//min = dist[j];
-	//k = j;
-	//}
-	//}
-	//// 标记"顶点k"为已经获取到最短路径
-	//flag[k] = 1;
-	//
-	//// 修正当前最短路径和前驱顶点
-	//// 即，当已经"顶点k的最短路径"之后，更新"未获取最短路径的顶点的最短路径和前驱顶点"。
-	//for (j = 0; j < G.vexnum; j++)
-	//{
-	//tmp = (G.matrix[k][j]==INF ? INF : (min + G.matrix[k][j])); // 防止溢出
-	//if (flag[j] == 0 && (tmp  < dist[j]) )
-	//{
-	//dist[j] = tmp;
-	//prev[j] = k;
-	//}
-	//}
-	//}
-	//
-	//// 打印dijkstra最短路径的结果
-	//printf("dijkstra(%c): \n", G.vexs[vs]);
-	//for (i = 0; i < G.vexnum; i++)
-	//printf("  shortest(%c, %c)=%d\n", G.vexs[vs], G.vexs[i], dist[i]);
 
-	return nil;
+/*
+ * Dijkstra最短路径。
+ * 即，统计图中"顶点vs"到其它各个顶点的最短路径。
+ *
+ * 参数说明：
+ *       vs -- 起始顶点(start vertex)。即计算"顶点vs"到其它顶点的最短路径。
+ *     prev -- 前驱顶点数组。即，prev[i]的值是"顶点vs"到"顶点i"的最短路径所经历的全部顶点中，位于"顶点i"之前的那个顶点。
+ *     dist -- 长度数组。即，dist[i]是"顶点vs"到"顶点i"的最短路径的长度。
+ */
+func Dijkstra(matrix [][]int, vs int) []int {
+
+	vexnum := len(matrix)
+	dist := make([]int, vexnum)
+	// flag[i]=true表示"顶点vs"到"顶点i"的最短路径已成功获取。用此变量可以区分S和U集合。
+	flag := make([]bool, vexnum)
+
+	// 初始化
+	for i := 0; i < vexnum; i++ {
+		// 顶点i的最短路径还没获取到。
+		flag[i] = false
+
+		// 顶点i的最短路径为"顶点vs"到"顶点i"的权。
+		dist[i] = matrix[vs][i];
+	}
+
+	// 对"顶点vs"自身进行初始化
+	flag[vs] = true
+	dist[vs] = 0
+
+	// 遍历G.vexnum-1次；每次找出一个顶点的最短路径。
+	for i := 1; i < vexnum; i++ {
+		// 寻找当前最小的路径；
+		// 即，在未获取最短路径的顶点中，找到离vs最近的顶点 k。
+		min := INF
+		var k int
+		for j := 0; j < vexnum; j++ {
+			if flag[j] == false {
+				if dist[j] < min {
+					min = dist[j]
+					k = j
+				}
+			}
+		}
+		// 标记"顶点k"为已经获取到最短路径
+		flag[k] = true
+		// 修正当前最短路径和前驱顶点
+		// 即，当已经"顶点k的最短路径"之后，更新"未获取最短路径的顶点的最短路径和前驱顶点"
+		for j := 0; j < vexnum; j++ {
+			var tmp = INF
+			if matrix[k][j] != INF {
+				tmp = min + matrix[k][j]
+			}
+			if flag[j] == false {
+				if tmp < dist[j] {
+					dist[j] = tmp
+				}
+			}
+		}
+	}
+
+	return dist
 }
--- structure/main/main.go
+++ structure/main/main.go
@@ -4,15 +4,20 @@ import (
 	"leetgo/structure/avl"
 	"leetgo/structure/link"
 	"leetgo/structure/btree"
+	"math"
+	"leetgo/structure/graph"
+	"fmt"
 )
 
-func main() {
+const INF = math.MaxInt32
 
+func testLinkList() {
 	list := link.CreateLinkList([]int{2, 1, 3, 4, 56, 2, 1})
 	link.PrintLinkList(list)
 
-	bt := btree.CreateBinaryTree([]int{1, 2, 3, 4}, 0)
-	btree.PrintBinaryTreeGraph(bt)
+}
+
+func testAVLTree() {
 
 	var root *avl.AVLTree
 
@@ -39,5 +44,26 @@ func main() {
 	avl.DeleteAVL(&root, 55)
 
 	avl.PrintLevelAVLTree(root)
+}
 
+func testBTree() {
+
+	bt := btree.CreateBinaryTree([]int{1, 2, 3, 4}, 0)
+	btree.PrintBinaryTreeGraph(bt)
+
+}
+
+func main() {
+	matrix := [][]int{
+		{0, 12, INF, INF, INF, 16, 14},
+		{12, 0, 10, INF, INF, 7, INF},
+		{INF, 10, 0, 3, 5, 6, INF},
+		{INF, INF, 3, 0, 4, INF, INF},
+		{INF, INF, 5, 4, 0, 2, 8},
+		{16, 7, 6, INF, 2, 0, 9},
+		{14, INF, INF, INF, 8, 9, 0},
+	}
+
+	dist := graph.Dijkstra(matrix, 3)
+	fmt.Printf("%v\n", dist)
 }
