4cc2f94b5bd8a78eac7b808ab75a7972c1164fa9
Zic <logzcx@126.com>
2017-11-29 00:12:34 +0800

    Finish a not perfect method.

---
+++ 101to200/137_Single_Number_II.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func singleNumber(nums []int) int {
+
+	N := len(nums)
+	var box = make(map[int]int)
+	for i := 0; i < N; i++ {
+		box[nums[i]]++
+	}
+	for k, v := range box {
+		if v == 1 {
+			return k
+		}
+	}
+
+	if N == 0 {
+		return 0
+	} else {
+		return nums[0]
+	}
+
+}
+
+func main() {
+
+	a := 2 ^ 2
+	fmt.Println(a)
+
+	fmt.Printf("%v\n", singleNumber([]int{1, 2, 2}))
+	fmt.Printf("%v\n", singleNumber([]int{2, 2, 3, 3, 5}))
+	fmt.Printf("%v\n", singleNumber([]int{1, 2, 2, 10, 1}))
+}
