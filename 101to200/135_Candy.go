f9998a7256435ef7ee6801445ec5a4c8fb926a77
Zic <logzcx@126.com>
2017-11-28 21:58:58 +0800

    Finish the 135 problem.

--- 101to200/135_Candy.go
+++ 101to200/135_Candy.go
@@ -5,15 +5,44 @@ import (
 )
 
 func candy(ratings []int) int {
-	var box = make(map[int]int)
-	for _, v := range ratings {
-		box[v]++
+	N := len(ratings)
+	if N == 0 {
+		return 0
+	}
+	var arr = make([]int, N)
+	arr[0] = 1
+	for i := 1; i < N; i++ {
+		if ratings[i-1] < ratings[i] {
+			arr[i] = arr[i-1] + 1
+		} else {
+			arr[i] = 1
+		}
 	}
 
-	return 1;
+	var max = func(a, b int) int {
+		if a > b {
+			return a
+		} else {
+			return b
+		}
+	}
+	for i := N - 1; i > 0; i-- {
+		if ratings[i-1] > ratings[i] {
+			arr[i-1] = max(arr[i-1], arr[i]+1)
+		}
+	}
+
+	sum := 0
+	for i := 0; i < N; i++ {
+		sum += arr[i]
+	}
+
+	return sum;
 }
 
 func main() {
 
-	fmt.Printf("%v\n", candy([]int{1, 2, 3, 4, 5}))
+	fmt.Printf("%v\n", candy([]int{1, 2, 3, 4}))
+	fmt.Printf("%v\n", candy([]int{2, 2, 3, 4}))
+	fmt.Printf("%v\n", candy([]int{1, 2, 2}))
 }
