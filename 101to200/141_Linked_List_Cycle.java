f8d548f1259b2cbf3bcb0e491b0531b9e18511ad
Zic <logzcx@126.com>
2017-11-29 22:17:55 +0800

    Finish 141.

---
+++ 101to200/141_Linked_List_Cycle.java
@@ -0,0 +1,25 @@
+package com.lish.practice.leetcode.p141;
+
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
