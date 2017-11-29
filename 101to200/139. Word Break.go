a464c855fc0f4400c1d06c274b48f131651edc95
zicla <lish516@126.com>
2017-11-29 14:11:34 +0800

    Finish issue 139 by a O(n^2) method.

--- 101to200/139. Word Break.go
+++ 101to200/139. Word Break.go
@@ -5,38 +5,27 @@ import (
 )
 
 func wordBreak(s string, wordDict []string) bool {
-	dictMap := make(map[string]int)
+	dictMap := make(map[string]bool)
 	for _, v := range wordDict {
-		dictMap[v] ++
+		dictMap[v] = true
 	}
 	N := len(s)
 
-	//dp[i][j]表示 startIndex=i endIndex=j 这个字符串从dictMap中多少种组合形式。我们要求的便是 dp[0][N-1]
-	var dp [][]int
-	for i := 0; i < N; i++ {
-		dp = append(dp, make([]int, N))
-	}
+	//dp[i]表示前i个字符串是否ok.
+	dp := make([]bool, N+1)
+	dp[0] = true
+	for i := 0; i <= N; i++ {
+		for j := 0; j < i; j++ {
 
-	for i := 0; i < N; i++ {
-		for j := i; j < N; j++ {
-			dp[i][j] = dictMap[s[i:j+1]]
-		}
-	}
-
-	for i := 0; i < N; i++ {
-		for j := i + 1; j < N; j++ {
-
-			for k := 0; k < j-i; k++ {
-				//x := dp[i][i+k]
-				//y := dp[i+k+1][j]
-				//fmt.Println(x)
-				//fmt.Println(y)
-				dp[i][j] += dp[i][i+k] * dp[i+k+1][j]
+			if dp[j] && dictMap[s[j:i]] {
+				dp[i] = true;
+				break
 			}
+
 		}
 	}
 
-	return dp[0][N-1] > 0
+	return dp[N]
 
 }
 
@@ -44,7 +33,7 @@ func main() {
 
 	fmt.Printf("%v\n", wordBreak("ab", []string{"a", "c"}))
 	fmt.Printf("%v\n", wordBreak("ab", []string{"a", "b"}))
-	fmt.Printf("%v\n", wordBreak("leetcode", []string{"leet1", "code"}))
+	fmt.Printf("%v\n", wordBreak("leetcode", []string{"leet", "code"}))
 
 	fmt.Printf("%v\n", wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", []string{"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"}))
 
