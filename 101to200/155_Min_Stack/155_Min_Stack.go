14484244a7a2348f102df705618bfbf45cd2a186
Zic <logzcx@126.com>
2017-12-03 00:44:30 +0800

    Finish the issue 155 with ...

--- 101to200/155_Min_Stack/155_Min_Stack.go
+++ 101to200/155_Min_Stack/155_Min_Stack.go
@@ -1,9 +1,15 @@
 package main
 
+import (
+	"math"
+	"fmt"
+)
+
 type MinStack struct {
-
+	stack []int
+	//min也维护一个类似栈的东西
+	min []int
 }
-
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
@@ -11,28 +17,40 @@ func Constructor() MinStack {
 	return MinStack{}
 }
 
+func (this *MinStack) Push(x int) {
 
-func (this *MinStack) Push(x int)  {
+	this.stack = append([]int{x}, this.stack...)
 
+	M := len(this.min)
+	if M == 0 || x <= this.min[0] {
+		this.min = append([]int{x}, this.min...)
+	}
 }
 
+func (this *MinStack) Pop() {
 
-func (this *MinStack) Pop()  {
+	if len(this.stack) == 0 {
+		return
+	}
+	top := this.stack[0]
+	this.stack = this.stack[1:]
 
+	if top == this.min[0] {
+		this.min = this.min[1:]
+	}
 }
-
 
 func (this *MinStack) Top() int {
-
-	return 0
+	if len(this.stack) == 0 {
+		return math.MinInt64
+	}
+	return this.stack[0]
 }
-
 
 func (this *MinStack) GetMin() int {
 
-	return 0
+	return this.min[0]
 }
-
 
 /**
  * Your MinStack object will be instantiated and called as such:
@@ -44,6 +62,16 @@ func (this *MinStack) GetMin() int {
  */
 
 func main() {
-
+	minStack := Constructor()
+	minStack.Push(512);
+	minStack.Push(-1024);
+	minStack.Push(-1024);
+	minStack.Push(512);
+	minStack.Pop();
+	fmt.Println(minStack.GetMin())
+	minStack.Pop();
+	fmt.Println(minStack.GetMin())
+	minStack.Pop();
+	fmt.Println(minStack.GetMin())
 
 }
