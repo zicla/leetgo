b4fe8d40b67c0622774981ef3329d5c294eac27e
Zic <logzcx@126.com>
2017-12-14 00:58:13 +0800

    Finish the issue 228.

--- 201to300/228_Summary_Ranges/main.go
+++ 201to300/228_Summary_Ranges/main.go
@@ -1,11 +1,37 @@
 package main
 
+import "fmt"
+
 func summaryRanges(nums []int) []string {
 
-	return nil
+	var res []string
+	N := len(nums)
+	if N == 0 {
+		return res
+	}
+
+	p := 0
+	for p < N {
+
+		q := p + 1
+		for q < N && nums[q]-nums[p] == q-p {
+			q++
+		}
+
+		if q-p == 1 {
+			res = append(res, fmt.Sprintf("%d", nums[p]))
+		} else {
+			res = append(res, fmt.Sprintf("%d->%d", nums[p], nums[q-1]))
+		}
+		p = q
+
+	}
+
+	return res
 }
 
 func main() {
 
-
+	fmt.Printf("%v\n", summaryRanges([]int{0, 1, 2, 4, 5, 7}))
+	fmt.Printf("%v\n", summaryRanges([]int{0, 2, 3, 4, 6, 8, 9}))
 }
