a2986b32ba74563bcd8e7b47651fb176c71c402f
Zic <logzcx@126.com>
2017-12-13 22:27:52 +0800

    Finish 226.

---
+++ 201to300/226_Invert_Binary_Tree/main.go
@@ -0,0 +1,26 @@
+package main
+
+import . "leetgo/structure/btree"
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+
+	return root
+
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
+func main() {
+
+}
