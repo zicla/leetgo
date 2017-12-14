914147b2650401590fc0be28032a8b11f2ebfbdc
zicla <lish516@126.com>
2017-12-14 10:57:30 +0800

    Finish the issue 229.

--- 201to300/229_Majority_Element_II/main.go
+++ 201to300/229_Majority_Element_II/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func majorityElement(nums []int) []int {
 	N := len(nums)
@@ -9,14 +12,44 @@ func majorityElement(nums []int) []int {
 		return res
 	}
 
-	dict := make(map[int]int)
-	for _, v := range nums {
-		dict[v]++
-	}
-	for k, v := range dict {
-		if v > N/3 {
-			res = append(res, k)
+	//摩尔投票法
+
+	m := math.MaxInt64
+	n := math.MaxInt64
+	cm := 0
+	cn := 0
+	for _, a := range nums {
+		if a == m {
+			cm++
+		} else if a == n {
+			cn++
+		} else if cm == 0 {
+			m = a
+			cm = 1
+		} else if cn == 0 {
+			n = a
+			cn = 1
+		} else {
+			cm--
+			cn--
 		}
+	}
+
+	cm = 0
+	cn = 0
+	for _, v := range nums {
+		if v == m {
+			cm++
+		} else if v == n {
+			cn++
+		}
+	}
+
+	if cm > N/3 {
+		res = append(res, m)
+	}
+	if cn > N/3 {
+		res = append(res, n)
 	}
 
 	return res
@@ -24,5 +57,8 @@ func majorityElement(nums []int) []int {
 
 func main() {
 
+	fmt.Printf("%v\n", majorityElement([]int{1, 1, 1, 2, 3, 4, 5, 6}))
 	fmt.Printf("%v\n", majorityElement([]int{1, 1, 2, 1, 2, 2}))
+	fmt.Printf("%v\n", majorityElement([]int{1, 1}))
+	fmt.Printf("%v\n", majorityElement([]int{0, 0, 0}))
 }
