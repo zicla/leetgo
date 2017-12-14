54bec1be4dd72b34192fcc54774ca8065b8a6764
Zic <logzcx@126.com>
2017-12-14 21:37:12 +0800

    Finish the things.

--- 201to300/239_Sliding_Window_Maximum/main.go
+++ 201to300/239_Sliding_Window_Maximum/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"math"
 	"fmt"
 )
 
@@ -12,20 +11,34 @@ func maxSlidingWindow(nums []int, k int) []int {
 	if N == 0 {
 		return res
 	}
-	for i := 0; i <= N-k; i++ {
-		max := math.MinInt64
-		for j := i; j < i+k; j++ {
-			if nums[j] > max {
-				max = nums[j]
+
+	// write your code here
+	var myDeq []int
+	for i := 0; i < N; i++ {
+		if i-k >= 0 {
+			// 出队列踢头节点判断
+			if len(myDeq) != 0 && nums[i-k] == myDeq[0] {
+				myDeq = myDeq[1:]
 			}
 		}
-		res = append(res, max)
+		// 入队列踢值判断
+		for len(myDeq) != 0 && nums[i] > myDeq[len(myDeq)-1] {
+			myDeq = myDeq[:len(myDeq)-1]
+		}
+		myDeq = append(myDeq, nums[i])
+		if i+1-k >= 0 {
+			// 窗口大小满足判断
+			res = append(res, myDeq[0])
+		}
+	}
+	if k > len(nums) {
+		res = append(res, myDeq[0])
 	}
 
 	return res
 }
 func main() {
 
-	fmt.Printf("%v\n", maxSlidingWindow([]int{}, 0))
 	fmt.Printf("%v\n", maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
+	fmt.Printf("%v\n", maxSlidingWindow([]int{}, 0))
 }
