9b5813d20784bc3ad08ac1b0dade94627f87ab0c
Zic <logzcx@126.com>
2017-12-16 00:08:24 +0800

    Finish 260 with a clever method.

--- 201to300/260_Single_Number_III/main.go
+++ 201to300/260_Single_Number_III/main.go
@@ -1,20 +1,31 @@
 package main
 
+import (
+	"fmt"
+)
+
 func singleNumber(nums []int) []int {
 
-	var dict = make(map[int]int)
-	for _, v := range nums {
-		dict[v]++
+	//全部异或起来就是 a^b 重要结论，0和任何数异或均为自己。
+	xor := 0
+	for i := 0; i < len(nums); i++ {
+		xor = xor ^ nums[i]
 	}
-	var res []int
-	for k, v := range dict {
-		if v == 1 {
-			res = append(res, k)
-		}
 
+	//找出a,b不相同的最低位。负数的表示是各位取反加1.
+	xor = xor & (-xor)
+	//此时借这一位分为两组。
+	var res = []int{0, 0}
+	for i := 0; i < len(nums); i++ {
+		if xor&nums[i] > 0 {
+			res[0] = res[0] ^ nums[i]
+		} else {
+			res[1] = res[1] ^ nums[i]
+		}
 	}
 	return res
 }
 func main() {
 
+	fmt.Printf("%v\n", singleNumber([]int{-1, 0}))
 }
